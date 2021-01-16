53f362bad9a87322a1d830290161d771cfc41fcd
Swapan Das <swapandas.home@outlook.com>
2021-01-16 01:35:56 -0500

    End of section 10

--- DatingApp/Controllers/UsersController.cs
+++ DatingApp/Controllers/UsersController.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Claims;
 using System.Threading.Tasks;
 using API.Data;
 using API.DTOs;
@@ -38,9 +39,22 @@ namespace API.Controllers
         [HttpGet("{username}")]
         public async Task<ActionResult<MemberDto>> GetUsers(string username)
         {
-            return await _userRepository.GetMemberAsync(username);
+            return await _userRepository.GetMemberAsync(username);            
+        }
 
-            
+        [HttpPut]
+        public async Task<ActionResult> UpdateUser(MemberUpdateDto memberUpdateDto)
+        {
+            var userName = User.FindFirst(ClaimTypes.NameIdentifier)?.Value;
+            var user = await _userRepository.GetUserByUserNameAsync(userName);
+            _mapper.Map(memberUpdateDto, user);
+
+            _userRepository.Update(user);
+
+            if (await _userRepository.SaveAllAsync()) return NoContent();
+
+            return BadRequest("Failed to update user");
+
         }
     }
 }
--- DatingApp/Helpers/AutoMapperProfiles.cs
+++ DatingApp/Helpers/AutoMapperProfiles.cs
@@ -19,6 +19,8 @@ namespace API.Helpers
                 .ForMember(desc => desc.Age, opt => opt.MapFrom(src => src.DateOfBirth.CalculateAge()));
 
             CreateMap<Photo, PhotoDto>();
+
+            CreateMap<MemberUpdateDto, AppUser>();
         }
     }
 }
